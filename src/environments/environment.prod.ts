fa77954a11fac2f9ec742eebb38d1d370b41b23a
ezizdemirov <eziz.demirov84@mail.com>
2022-05-19 15:41:39 +0400

    root

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  rootUrl: 'https://localhost:44342/api/',
+  // rootUrl: 'https://localhost:44342/api/',
+  rootUrl: 'http://demoaziz-001-site1.itempurl.com/',
 };
